37bb8fef396f6834b2ebc4524ec506a06ef84b4e
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-10-02 20:42:10 +0600

    product, image, line

--- product/serializers.py
+++ product/serializers.py
@@ -1,24 +1,54 @@
 from rest_framework import serializers
 
-from .models import Brand, Category, Product
+from .models import Brand, Category, Product, ProductImage, ProductLine
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source="name")
+
     class Meta:
         model = Category
-        fields = ["name"]
+        fields = ["category_name"]
 
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
-        fields = "__all__"
+        exclude = ("id",)
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        exclude = ("id", "productline")
+
+
+class ProductLineSerializer(serializers.ModelSerializer):
+    product_image = ProductImageSerializer(many=True)
+
+    class Meta:
+        model = ProductLine
+        fields = (
+            "price",
+            "sku",
+            "stock_qty",
+            "order",
+            "product_image",
+        )
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    brand = BrandSerializer()
-    category = CategorySerializer()
+    brand_name = serializers.CharField(source="brand.name")
+    category_name = serializers.CharField(source="category.name")
+    product_line = ProductLineSerializer(many=True)
 
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = (
+            "name",
+            "slug",
+            "description",
+            "brand_name",
+            "category_name",
+            "product_line",
+        )
--- product/views.py
+++ product/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from sqlparse import format
+from pygments import highlight
+from django.db import connection
+from pygments.lexers import SqlLexer
 from rest_framework import viewsets
-from drf_spectacular.utils import extend_schema
+from django.db.models import Prefetch
 from rest_framework.response import Response
+from rest_framework.decorators import action
+from drf_spectacular.utils import extend_schema
+from pygments.formatters import TerminalFormatter
 
 from .models import Brand, Category, Product
 from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
@@ -38,9 +44,39 @@ class ProductViewSet(viewsets.ViewSet):
     A simple Viewset for viewing all products
     """
 
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().isactive()
+    lookup_field = "slug"
+
+    def retrieve(self, request, slug=None):
+        serializer = ProductSerializer(
+            self.queryset.filter(slug=slug).select_related("category", "brand"),
+            many=True,
+        )
+        data = Response(serializer.data)
+        # print
+        q = list(connection.queries)
+        print(len(q))
+        for qs in q:
+            sqlformatted = format(str(qs["sql"]), reindent=True)
+            print(highlight(sqlformatted, SqlLexer(), TerminalFormatter()))
+
+        return data
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<slug>[\w-]+)",
+    )
+    def list_product_by_category_slug(self, request, slug=None):
+        """
+        An endpoint to return products by category
+        """
+        serializer = ProductSerializer(
+            self.queryset.filter(category__slug=slug), many=True
+        )
         return Response(serializer.data)
